59589303774fb85cd07a7a7d5e89565533c3f0d3
gicunho <gicunho@hotmail.it>
2021-05-06 19:45:49 +0200

    Esercizio finito con le console.log()

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,45 +5,41 @@ function generatoreNumeriCasuali(min, max) {
 
 var numeri = [];
 for (i = 0; i < 5; i++) {
-    numero = generatoreNumeriCasuali(1, 10);
+    numero = generatoreNumeriCasuali(1, 100);
     numeri.push(numero);
 }
 console.log('numeri generati = ' + numeri);
 alert('I 5 numeri da memorizzare sono' + numeri)
+
 //Da li parte un timer di 30 secondi.
-var numeriUtente = [];
-var numeriGiusti = [];
-var numeriSbagliati = [];
+var numeriUtente = [],
+    numeriGiusti = [],
+    numeriSbagliati = [];
 
 setTimeout(function () {
+    //Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
     for (j = 0; j < 5; j++) {
-        var numeroUtente = prompt('Inserisci un numero');
+        var numeroUtente = Number(prompt('Inserisci un numero'));
         numeriUtente.push(numeroUtente);
     }
-    
-    var numeriNew = numeri;
-    var numeriUtenteNew = numeriUtente
 
+    //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
     for (y = 0; y < 5; y++) {
-        if (numeriNew.includes(numeriUtenteNew[y])) {
-            numeriGiusti.push(numeriUtenteNew[y]);
-        } else {
-            numeriSbagliati.push(numeriUtenteNew[y]);
+        for (z = 0; z < 5; z++ ) {
+            if (numeri[y] === numeriUtente[z] && !numeriGiusti.includes(numeriUtente[z])) {
+                numeriGiusti.push(numeriUtente[z])
+            }
         }
-    } 
-    console.log('numeri scelti da utente =' + numeriUtenteNew);
-        console.log('numeri giusti ' + numeriGiusti);
-        console.log('numeri sbagliati ' + numeriSbagliati);
+    }
+
+    var messaggio;
+    if (numeriGiusti.length > 0) {
+        messaggio = `Complimenti, hai indovinato ${numeriGiusti.length} numeri!
+Eccoli: ${numeriGiusti}`
+    } else {
+        messaggio = `Purtroppo non hai indovinato nessun numero`
+    }
+    console.log('numeri scelti da utente =' + numeriUtente);
+    console.log('numeri giusti ' + numeriGiusti);
+    console.log(messaggio);
 }, 2000)
-
-
-
-
-//Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-
-/* 
-Consigli del giorno:
-* Pensate prima in italiano.
-* Dividete in piccoli problemi la consegna.
-* Individuate gli elementi di cui avete bisogno per realizzare il programma. */
