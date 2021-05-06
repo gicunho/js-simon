d89b165b2b833fbe90ad513a8349deebd6fc8be2
gicunho <gicunho@hotmail.it>
2021-05-06 18:07:27 +0200

    Aggiunto setTimeout

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,37 @@ for (i = 0; i < 5; i++) {
     numero = generatoreNumeriCasuali(1, 10);
     numeri.push(numero);
 }
-console.log(numeri);
+console.log('numeri generati = ' + numeri);
 alert('I 5 numeri da memorizzare sono' + numeri)
 //Da li parte un timer di 30 secondi.
+var numeriUtente = [];
+var numeriGiusti = [];
+var numeriSbagliati = [];
+
+setTimeout(function () {
+    for (j = 0; j < 5; j++) {
+        var numeroUtente = prompt('Inserisci un numero');
+        numeriUtente.push(numeroUtente);
+    }
+    
+    var numeriNew = numeri;
+    var numeriUtenteNew = numeriUtente
+
+    for (y = 0; y < 5; y++) {
+        if (numeriNew.includes(numeriUtenteNew[y])) {
+            numeriGiusti.push(numeriUtenteNew[y]);
+        } else {
+            numeriSbagliati.push(numeriUtenteNew[y]);
+        }
+    } 
+    console.log('numeri scelti da utente =' + numeriUtenteNew);
+        console.log('numeri giusti ' + numeriGiusti);
+        console.log('numeri sbagliati ' + numeriSbagliati);
+}, 2000)
+
+
+
+
 //Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
