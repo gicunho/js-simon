51118ee748b0feeb22d4c782568b888feda82f20
gicunho <gicunho@hotmail.it>
2021-05-06 20:31:37 +0200

    Aggiunto Timer Countdown su HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,46 @@
-//Un alert() espone 5 numeri generati casualmente.
+/**
+ * ##Generatore numeri casuali
+ * Funzione per generare un numero casuale tra un minimo ed un massimo
+ * @param {number} min - numero minimo che genererà
+ * @param {number} max - numero massimo che genererà
+ * @returns {number}
+ */
 function generatoreNumeriCasuali(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+//Generazione e push nell'array i numeri casuali con la funzione
+//Generato numeri tra 1 e 100
 var numeri = [];
 for (i = 0; i < 5; i++) {
     numero = generatoreNumeriCasuali(1, 100);
     numeri.push(numero);
 }
-console.log('numeri generati = ' + numeri);
+
+//Un alert() espone 5 numeri generati casualmente.
 alert('I 5 numeri da memorizzare sono' + numeri)
 
 //Da li parte un timer di 30 secondi.
+
+var countdown = 30;
+var timer = setInterval(function () {
+    var tempoEl = document.getElementById('timer');
+    countdown--;
+    tempoEl.innerHTML = countdown;
+    if (countdown === 0) {
+        tempoEl.innerHTML = "Il gioco inizia ora";
+        clearInterval(timer);
+    }
+}, 1000);
+
+
 var numeriUtente = [],
-    numeriGiusti = [],
-    numeriSbagliati = [];
+    numeriGiusti = [];
 
 setTimeout(function () {
     //Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
     for (j = 0; j < 5; j++) {
-        var numeroUtente = Number(prompt('Inserisci un numero'));
+        var numeroUtente = Number(prompt('Inserisci un numero dei numeri di prima senza ripeterlo'));
         numeriUtente.push(numeroUtente);
     }
 
@@ -33,13 +54,23 @@ setTimeout(function () {
     }
 
     var messaggio;
-    if (numeriGiusti.length > 0) {
+    //Se 1 numero inserito è giusto
+    if (numeriGiusti.length = 1) {
+        messaggio = `Bravo, hai indovinato 1 numero!
+Eccolo: ${numeriGiusti}`
+    }
+    //Se più di 1 numero inserito è giusto
+
+    else if (numeriGiusti.length > 1) {
         messaggio = `Complimenti, hai indovinato ${numeriGiusti.length} numeri!
 Eccoli: ${numeriGiusti}`
-    } else {
+    } 
+    //Se nessun numero inserito è giusto
+    
+    else {
         messaggio = `Purtroppo non hai indovinato nessun numero`
     }
     console.log('numeri scelti da utente =' + numeriUtente);
     console.log('numeri giusti ' + numeriGiusti);
     console.log(messaggio);
-}, 2000)
+}, 30000)
